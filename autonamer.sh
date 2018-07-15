94763335efa2483f5b1b7b993a49033911e95ae1
Max Zhao <alcasa.mz@gmail.com>
2018-07-15 14:25:07 +0200

    initial commit

---
+++ autonamer.sh
@@ -0,0 +1,43 @@
+#!/bin/dash
+
+# Set the desktop name based on the activating window class
+# param1: window class
+# param2 (opt): window id (used for some title lookups)
+set_name() {
+	case $1 in
+		Firefox)
+			name="Browser"
+			;;
+		Thunderbird)
+			name="Mail"
+			;;
+		Nautilus)
+			name="Files"
+			;;
+		*)
+			echo "Class $1 not found" > /tmp/bspc-external-rename
+			return
+			;;
+	esac
+	bspc desktop -n $name
+}
+
+wid=$1
+class=$(echo ${2} | xargs)
+instance=$(echo ${3} | xargs)
+# title=$(xtitle "$wid")
+
+# Debug
+echo "$wid $class $instance" > /tmp/bspc-external-rules
+
+# name the desktop if it doesnt already have a name
+desktop_name=$(bspc query -D -d --names)
+case $desktop_name in
+	[0-9]*)
+		echo "Empty, rename." > /tmp/bspc-external-rename
+		set_name $class $wid
+		;;
+	*)
+		echo "Full, do not rename." > /tmp/bspc-external-rename
+		;;
+esac
---
+++ autoremover.sh
@@ -0,0 +1,49 @@
+#!/bin/dash
+# script to automatically reset desktop name after last node exited
+
+reset_desktop_name() {
+	monitor=$1
+	desktop=$2
+	node=$3
+
+	# check that the desktop is empty before removing name
+	bspc query -N -d $desktop > /dev/null 2>&1
+	if [ $? -eq 1 ]; then
+		desktop_number=$(bspc query -D -m $monitor | grep -no $desktop | cut -d : -f 1)
+		bspc desktop $desktop -n "$desktop_number"
+	fi
+}
+
+# set name on desktop if it is the only window
+# arg1: desktop id
+# arg2: new desktop name
+set_desktop_name() {
+	# check that the desktop has only one node
+	if [ $(bspc query -N -n .window -d $1 | wc -l) -eq 1 ]; then
+		bspc desktop $1 -n $2
+	fi
+}
+
+# swap node into a different desktop, check whether the source is now empty
+swap_nodes() {
+	# args: src_monitor src_desktop src_node dst_monitor dst_desktop dst_node
+	cur_source_name=$(bspc query -D -d $2 --names)
+	# remove name from old desktop if now empty
+	reset_desktop_name $1 $2 $3
+	# add name to new desktop if it was empty
+	set_desktop_name $5 $cur_source_name
+}
+
+bspc subscribe node | while read etype eargs
+do
+	case $etype in
+		# clear desktop name on removal
+		node_remove)
+			reset_desktop_name $eargs
+			;;
+		# clear source desktop on moving nodes
+		node_transfer)
+			swap_nodes $eargs
+			;;
+	esac
+done
